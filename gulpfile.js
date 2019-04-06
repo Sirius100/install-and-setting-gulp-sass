c6b26bb332b2b298acf4fbca7bc4658e2fb9eaf0
One project <esaul-00@mail.ru>
2019-04-06 15:46:06 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,40 @@
 const gulp = require('gulp');
 const sass = require('gulp-ruby-sass');
 const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const prefixer = require('gulp-autoprefixer');
 
+
+
+//команда запуска препроцессора sass
+//создает сss файл из sass файла
 gulp.task('sass', () =>
-    sass('source/file.scss', {sourcemap: true})
-        .on('error', sass.logError)
-        // for inline sourcemaps
-        .pipe(sourcemaps.write())
-        // for file sourcemaps
-        .pipe(sourcemaps.write('maps', {
-            includeContent: false,
-            sourceRoot: 'source'
-        }))
-        .pipe(gulp.dest('./css'))
+  sass('**/sass/*.sass')
+  .on('error', sass.logError)
+);
+
+//вывод в консоль предупреждения об изменении файла sass
+gulp.task('startnote', () =>
+  console.log('***!!! <СТАРТ трансформации> !!!***')
+);
+
+gulp.task('endnote', () =>
+  console.log('***!!! <КОНЕЦ трансформации> !!!***')
+);
+
+//объединение файлов *.css in style.css
+gulp.task('concat', () =>
+  gulp.src('./src/sass/*.css')
+    .pipe(prefixer()) //вызов плагина расстановки префиксов
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('src/css'))
+);
+
+gulp.task('watch', () =>
+  gulp.watch('src/sass/*.sass', ['sass', 'concat'])
 );
 
 
+// var watcher = gulp.watch('src/sass/*.sass', ['endnote']);
+
+gulp.task('default', ['startnote', 'watch', 'sass', 'concat', 'endnote']);
