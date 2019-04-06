4efe09b07fd27f57f5218c304279e194c03c7741
One project <esaul-00@mail.ru>
2019-04-07 00:52:21 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,31 @@
-const gulp = require('gulp');
-const sass = require('gulp-ruby-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const concat = require('gulp-concat');
-const prefixer = require('gulp-autoprefixer');
+const gulp            = require('gulp');
+const sass            = require('gulp-ruby-sass');
+const concat          = require('gulp-concat');
+const prefixer        = require('gulp-autoprefixer');
 
+
+
+// //объединение файлов *.sass in style.sass
+gulp.task('concat', function () {
+   return gulp.src('../src/sass/css/*.css')
+       .pipe(concat('style.css'))
+       .pipe(gulp.dest('../css'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('../src/sass/*.sass', ['sass']);
+})
 
 
 //команда запуска препроцессора sass
 //создает сss файл из sass файла
-gulp.task('sass', () =>
-  sass('**/sass/*.sass')
-  .on('error', sass.logError)
-);
+gulp.task('sass', function () {
+    sass('../src/sass/*.sass')
+    .on('error', sass.logError)
+        .pipe(gulp.dest('../src/sass/css/'))
+            .pipe(concat('style.css'))
+            .pipe(prefixer())
+            .pipe(gulp.dest('../css'))
+});
 
-//вывод в консоль предупреждения об изменении файла sass
-gulp.task('startnote', () =>
-  console.log('***!!! <СТАРТ трансформации> !!!***')
-);
-
-gulp.task('endnote', () =>
-  console.log('***!!! <КОНЕЦ трансформации> !!!***')
-);
-
-//объединение файлов *.css in style.css
-gulp.task('concat', () =>
-  gulp.src('./src/sass/*.css')
-    .pipe(prefixer()) //вызов плагина расстановки префиксов
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('src/css'))
-);
-
-gulp.task('watch', () =>
-  gulp.watch('src/sass/*.sass', ['sass', 'concat'])
-);
-
-
-// var watcher = gulp.watch('src/sass/*.sass', ['endnote']);
-
-gulp.task('default', ['startnote', 'watch', 'sass', 'concat', 'endnote']);
+gulp.task('default',['watch']);
